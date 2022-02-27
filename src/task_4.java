cd6ffab7b5eb3a98dd3eccd8aa2294d637da5c1d
AlexRusV <aleksej-rusak@mail.ru>
2022-02-27 22:17:10 +0300

    Task-4

---
+++ src/task_4.java
@@ -0,0 +1,12 @@
+public class task_4 {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 7;
+        int a = x + y;
+        int b = x * y;
+        System.out.println(a + b);
+    }
+        static int sum( int a, int b){
+            return a + b;
+        }
+    }
